24e52d52e4140ff71b546a802b08fc242f0484e3
Lorrrany333 <135670048+Lorrrany333@users.noreply.github.com>
2023-10-16 19:45:52 -0300

    u

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function tocaSomTim () {
   document.querySelector('#so_tecla_tim').play();
 
 }
- document.querySelector('.tecla_tim').onclick =tocaSomTim
+ document.querySelector('.tecla_tim').onclick = tocaSomTim
 
 
  function tocaSomPuff () {
@@ -16,16 +16,34 @@ function tocaSomTim () {
 
 
  }
- document.querySelector('tecla_puff').onclick =tocaSomPuff
+ document.querySelector('tecla_puff').onclick = tocaSomPuff
 
 
  function tocaSomClap () {
    document.querySelector('#toca_tecla_clap').play();
 
  }
- document.querySelector('.tecla_clap').onclick =tocaSomClap
+ document.querySelector('.tecla_clap').onclick = tocaSomClap
 
 
  function tocaSomToim () {
-  document.querySelector('#toca_tecla_toim')
+  document.querySelector('#toca_tecla_toim').play();
+
  }
+ document.querySelector('.tecla_toim').onclick = tocaSomToim
+
+
+ function tocaSomSplash () {
+  document.querySelector('#toca_tecla_splash').play();
+
+ }
+ document.querySelector('.tecla_splash').onclick = tocaSomSplash
+   
+
+ function tocaSomPuff () {
+  document.querySelector('#toca_tecla_puff').play ();
+
+}
+ document.querySelector('.tecla_puff').onclick = tocaSomPuff
+
+ 
