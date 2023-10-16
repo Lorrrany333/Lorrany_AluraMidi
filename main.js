a5359df87e9a93d0a0aa141f963fe996e260e3da
Lorrrany333 <135670048+Lorrrany333@users.noreply.github.com>
2023-10-16 20:04:13 -0300

    s

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ function tocaSomPom () {
 document.querySelector('.tecla_pom').onclick = tocaSomPom
 
 
-function tocaSomTim () {
-  document.querySelector('#som_tecla_tim').play();
+function tocaSomClap () {
+  document.querySelector('#som_tecla_clap').play();
 }
- document.querySelector('.tecla_tim').onclick = tocaSomTim
+ document.querySelector('.tecla_clap').onclick = tocaSomClap
 
 
  function tocaSomPuff () {
